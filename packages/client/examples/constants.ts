89babe8123cfe0987b5d3abae5118d74b4ea5d8f
bielwarm <gabrielvmwi@gmail.com>
2025-03-15 20:34:24 +0100

    Add initial examples folder and structure

---
+++ packages/client/examples/constants.ts
@@ -0,0 +1,9 @@
+import { address } from "@solana/kit";
+
+export const MIN_COMPUTE_UNIT_PRICE = 50_000;
+export const MAX_CU_LIMIT = 1_400_000;
+export const WSOL_MINT = address("So11111111111111111111111111111111111111112");
+export const SOL_USDC_WHIRLPOOL = address("Czfq3xZZDmsdGdUyrNLtRhGc47cXcZtLG4crryfu44zE");
+
+export const DEFAULT_RPC_URL = "https://api.mainnet-beta.solana.com";
+export const DEFAULT_WSS_URL = "wss://api.mainnet-beta.solana.com/";
