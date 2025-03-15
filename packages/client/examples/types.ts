8a6a33a95a423adc50118b263e8c57e0e8dfe8fa
bielwarm <gabrielvmwi@gmail.com>
2025-03-15 20:34:24 +0100

    Add initial examples folder and structure

---
+++ packages/client/examples/types.ts
@@ -0,0 +1,19 @@
+import { Address, IInstruction } from "@solana/kit";
+
+export type TokenMint = Address & {
+  [isTokenMint: symbol]: true;
+};
+
+export type WalletAddress = Address & {
+  [isWalletAddress: symbol]: true;
+};
+
+export type CreateAndDepositLendingPositionInstructions = {
+  depositLendingIx: IInstruction;
+  createLendingPositionIx: IInstruction | null;
+  createAtaIx: IInstruction | null;
+  wSolAtaIxs: IInstruction[];
+  closeWSolAtaIx: IInstruction | null;
+};
+
+export type AtaInstructions = { createAtaIxs: IInstruction[]; closeWSolAtaIxs: IInstruction[] };
